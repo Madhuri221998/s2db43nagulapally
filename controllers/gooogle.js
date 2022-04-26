4ad67e410a4f89662008f4a43abfa6fe59351b1f
Madhuri221998 <madzzna12@gmail.com>
2022-04-25 20:28:03 -0500

    made changes at the local repo

--- controllers/gooogle.js
+++ controllers/gooogle.js
@@ -1,10 +1,10 @@
 var gooogle = require('../models/gooogle');
 
 // List of all gooogle
-exports.gooogle_list = async function(req, res) {
+exports.house_list = async function(req, res) {
     try{
-    thegooogle = await gooogle.find();
-    res.send(thegooogle);
+    thehouse = await gooogle.find();
+    res.send(thehouse);
     }
     catch(err){
     res.status(500);
@@ -12,11 +12,11 @@ exports.gooogle_list = async function(req, res) {
     }
    };
 
-//exports gooogle_list = function(req, res) {
+//exports house_list = function(req, res) {
  //res.send('NOT IMPLEMENTED: gooogle list');
 //};
 // for a specific gooogle.
-exports.gooogle_detail = async function(req, res) {
+exports.house_detail = async function(req, res) {
  //res.send('NOT IMPLEMENTED: gooogle detail: ' + req.params.id);
  console.log("detail"  + req.params.id) 
  try { 
@@ -30,16 +30,15 @@ exports.gooogle_detail = async function(req, res) {
 };
 // Handle gooogle create on POST.
 // Handle gooogle create on POST.
-exports.gooogle_create_post = async function(req, res) {
+exports.house_create_post = async function(req, res) {
     console.log(req.body)
     let document = new gooogle();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many diffeprice formats, we will be picky
     // and require that it be a json object
-    // {"gooogle_type":"goat", "cost":12, "size":"large"}
-    document.gooogle_itemname = req.body.gooogle_itemname;
-    document.gooogle_quantity = req.body.gooogle_quantity;
-    document.gooogle_price = req.body.gooogle_price;
+        document.house_itemname = req.body.house_itemname;
+    document.house_quantity = req.body.house_quantity;
+    document.house_price = req.body.house_price;
     try{
     let result = await document.save();
     res.send(result);
@@ -50,38 +49,39 @@ exports.gooogle_create_post = async function(req, res) {
     }
    };
 // Handle gooogle delete form on DELETE.
-//exports.gooogle_delete = function(req, res) {
+//exports.house_delete = function(req, res) {
  //res.send('NOT IMPLEMENTED: gooogle delete DELETE ' + req.params.id);
 //};
 // Handle gooogle delete on DELETE. 
-exports.gooogle_delete = async function(req, res) { 
-    console.log("delete "  + req.params.id) 
-    try { 
-        result = await gooogle.findByIdAndDelete( req.params.id) 
-        console.log("Removed " + result) 
-        res.send(result) 
-    } catch (err) { 
-        res.status(500) 
-        res.send(`{"error": Error deleting ${err}}`); 
-    } 
-}; 
+// Handle gooogle delete on DELETE.
+exports.house_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await gooogle.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
 // Handle gooogle update form on PUT.
-//exports.gooogle_update_put = function(req, res) {
+//exports.house_update_put = function(req, res) {
  //res.send('NOT IMPLEMENTED: gooogle update PUT' + req.params.id);
 //};
 
 //Handle gooogle update form on PUT. 
-exports.gooogle_update_put = async function(req, res) { 
+exports.house_update_put = async function(req, res) { 
     console.log(`update on id ${req.params.id} with body 
 ${JSON.stringify(req.body)}`) 
     try { 
         let toUpdate = await gooogle.findById( req.params.id) 
         console.log(req.body);
         // Do updates of properties 
-        if(req.body.gooogle_itemname)  
-               toUpdate.gooogle_itemname = req.body.gooogle_itemname; 
-        if(req.body.gooogle_quantity) toUpdate.gooogle_quantity = req.body.gooogle_quantity; 
-        if(req.body.gooogle_price) toUpdate.gooogle_price = req.body.gooogle_price; 
+        if(req.body.house_itemname)  
+               toUpdate.house_itemname = req.body.house_itemname; 
+        if(req.body.house_quantity) toUpdate.house_quantity = req.body.house_quantity; 
+        if(req.body.house_price) toUpdate.house_price = req.body.house_price; 
         let result = await toUpdate.save(); 
         console.log("Sucess " + result) 
         res.send(result) 
@@ -93,14 +93,70 @@ ${JSON.stringify(req.body)}`)
 
 // VIEWS
 // Handle a show all view
-exports.gooogle_view_all_Page = async function(req, res) {
+exports.house_view_all_Page = async function(req, res) {
     try{
         console.log('test'+gooogle.find())
-    thegooogle = await gooogle.find();
-    res.render('gooogle', { title: 'gooogle Search Results', results: thegooogle });
+    thehouse = await gooogle.find();
+    res.render('gooogle', { title: 'gooogle Search Results', results: thehouse });
     }
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
+    }
+   };
+
+   // Handle a show one view with id specified by query
+exports.house_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await gooogle.findById( req.query.id)
+    res.render('housedetail',
+   { title: 'gooogle Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for creating a gooogle.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.house_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('housecreate', { title: 'gooogle Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a gooogle.
+// query provides the id
+exports.house_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await gooogle.findById(req.query.id)
+    res.render('houseupdate', { title: 'gooogle Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle a delete one view with id from query
+exports.house_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await gooogle.findById(req.query.id)
+    res.render('housedelete', { title: 'gooogle Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
    };
