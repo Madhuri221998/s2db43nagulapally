d0c45238911606d6dd375d032c514fe0b34e4e61
Madhuri221998 <madzzna12@gmail.com>
2022-04-25 23:24:41 -0500

    made changes at the local repo

--- routes/gooogle.js
+++ routes/gooogle.js
@@ -1,6 +1,30 @@
 var express = require('express');
-const gooogle_controlers= require('../controllers/gooogle');
+const house_controlers= require('../controllers/house');
 var router = express.Router();
-/* GET gooogle */
-router.get('/', gooogle_controlers.gooogle_view_all_Page );
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
+/* GET house */
+router.get('/', house_controlers.house_view_all_Page );
 module.exports = router;
+
+/* GET detail house page */
+router.get('/detail', house_controlers.house_view_one_Page);
+
+/* GET create house page */
+router.get('/create', secured, house_controlers.house_create_Page);
+
+/* GET create update page */
+router.get('/update', secured, house_controlers.house_update_Page);
+
+/* GET delete house page */
+router.get('/delete', secured, house_controlers.house_delete_Page);
+
+
+
+/* GET update house page */ 
+router.get('/update', house_controlers.house_update_Page);
